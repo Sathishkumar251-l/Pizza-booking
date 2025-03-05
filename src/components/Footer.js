d2661783c2f8324846a5dbc743d0f6118960bf71
Sathish <manisathish751@gmail.com>
2025-03-05 18:51:22 +0530

    .
    .

---
+++ src/components/Footer.js
@@ -0,0 +1,23 @@
+import React from "react";
+import InstagramIcon from '@mui/icons-material/Instagram';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+// import InstagramIcon from "@material-ui/icons/Instagram";
+// import TwitterIcon from "@material-ui/icons/Twitter";
+// import FacebookIcon from "@material-ui/icons/Facebook";
+// import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import "../styles/Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="socialMedia">
+        <InstagramIcon /> <TwitterIcon /> <FacebookIcon /> <LinkedInIcon />
+      </div>
+      <p> &copy; 2025 crustifypizza.com</p>
+    </div>
+  );
+}
+
+export default Footer;
